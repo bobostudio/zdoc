e67e39dcf4ccde394b4bc0445096491eeb259745
bobostudio <m18576493832@163.com>
2024-03-17 23:48:32 +0800

    feat: add tag,xml,and island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
+import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
-import { defineConfig } from 'astro/config'
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://localhost:4321/",
+  integrations: [preact()]
+});
